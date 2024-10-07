2f7d7e74d6e37419a763bcf44550c467cd91ec3c
Gustavo-Bayer <sevengusta@gmail.com>
2024-10-07 14:11:43 -0300

    adding photos

--- signs_recognition/application.py
+++ signs_recognition/application.py
@@ -1,18 +1,20 @@
 import streamlit as st
-import tensorflow as tf
+import pandas as pd 
+# import tensorflow as tf
 
-img_file_buffer = st.camera_input("Take a picture")
+st.write("Uma página em branco")
+# img_file_buffer = st.camera_input("Take a picture")
 
-if img_file_buffer is not None:
-    # To read image file buffer as a 3D uint8 tensor with TensorFlow:
-    bytes_data = img_file_buffer.getvalue()
-    img_tensor = tf.io.decode_image(bytes_data, channels=3)
+# if img_file_buffer is not None:
+#     # To read image file buffer as a 3D uint8 tensor with TensorFlow:
+#     bytes_data = img_file_buffer.getvalue()
+#     img_tensor = tf.io.decode_image(bytes_data, channels=3)
 
-    # Check the type of img_tensor:
-    # Should output: <class 'tensorflow.python.framework.ops.EagerTensor'>
-    st.write(type(img_tensor))
+#     # Check the type of img_tensor:
+#     # Should output: <class 'tensorflow.python.framework.ops.EagerTensor'>
+#     st.write(type(img_tensor))
 
-    # Check the shape of img_tensor:
-    # Should output shape: (height, width, channels)
-    st.write(img_tensor.shape)
-    st.write(img_tensor.shape)
+#     # Check the shape of img_tensor:
+#     # Should output shape: (height, width, channels)
+#     st.write(img_tensor.shape)
+#     st.write(img_tensor.shape)
